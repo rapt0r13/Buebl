fa3a00dd9fff4ee9fbfc84043b2e6e8cde5c364e
Tom <napstertom@gmail.com>
2016-06-22 09:34:29 +0200

    bueblDrive

--- src/bublrobot/Constants.java
+++ src/bublrobot/Constants.java
@@ -1,25 +1,20 @@
 package bublrobot;
 
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
-import lejos.robotics.RegulatedMotor;
 
-public class bueblDrive {
+public class Constants {
 
+
+	public static double WHEEL_DIAMETER = 0.00685;
+	public static double WHEEL_DISTANCE = 0;
+	public static double WHEEL_THICK = 0;
 	final static int DRIVE_MOTOR_ACCELERATION = 200;
 	final static int DRIVE_MOTOR_SPEED = 100;
 	final static int REGULATED_MOTOR_POWER = 75;
 	final static Port DRIVE_MOTOR_PORT = MotorPort.C; //steering Motor C
 	
-	
-	/**
-	 * Move straight 0.5m
-	 */
-	
-	public void moveForward() {
-		int degrees = Math.round(());
+		 
 	}
-	
-}
+
+
