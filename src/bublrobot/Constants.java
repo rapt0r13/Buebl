52c6241929f0a068e29f647e1c099b11cd5847b4
Tom <napstertom@gmail.com>
2016-06-22 14:42:32 +0200

    MovePilot instead of DifferentialPilot

--- src/bublrobot/Constants.java
+++ src/bublrobot/Constants.java
@@ -6,9 +6,9 @@ import lejos.hardware.port.Port;
 public class Constants {
 
 
-	public static double WHEEL_DIAMETER = 6.85;
-	public static double TRACK_WIDTH = 16.2;
-	public static double WHEEL_THICK = 2.8;
+	public static double WHEEL_DIAMETER = 6.9;
+	public static double TRACK_WIDTH = 11.6;
+	public static double WHEEL_THICK = 3.5;
 	final static int DRIVE_MOTOR_ACCELERATION = 200;
 	final static int DRIVE_MOTOR_SPEED = 100;
 	final static int REGULATED_MOTOR_POWER = 75;
--- src/bublrobot/bueblDrive.java
+++ src/bublrobot/bueblDrive.java
@@ -2,8 +2,13 @@ package bublrobot;
 
 import lejos.hardware.Sound;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.robotics.navigation.*;
-import lejos.utility.Delay;
+
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+
+import lejos.robotics.navigation.MovePilot;
+
 
 public class bueblDrive {
 
@@ -17,20 +22,17 @@ public class bueblDrive {
 	public void moveForward() {
 		EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(Constants.DRIVE_MOTOR_RIGHT);
 		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(Constants.DRIVE_MOTOR_LEFT);
-		Sound.beepSequence(); 
-		DifferentialPilot pilot = new DifferentialPilot(Constants.WHEEL_DIAMETER, 
-				Constants.WHEEL_DIAMETER, Constants.TRACK_WIDTH, leftMotor, rightMotor, true);
-		Sound.beepSequence(); 
-		 pilot.travel(50);         // cm
+		 Wheel wheel1 = WheeledChassis.modelWheel(rightMotor, 43.2).offset(-72);
+		 Wheel wheel2 = WheeledChassis.modelWheel(leftMotor, 43.2).offset(72);
+		 Chassis chassis = new WheeledChassis(new Wheel[]{wheel1, wheel2}, WheeledChassis.TYPE_DIFFERENTIAL); 
+		 MovePilot pilot = new MovePilot(chassis);
+		 Sound.beepSequenceUp();
+		 pilot.travel(1000);         // cm
 		 pilot.rotate(-90);        // degree clockwise
-		 pilot.travel(-50,true);  //  move backward for 50 cm
+		 pilot.travel(-100,true);  //  move backward for 50 cm
 		 while(pilot.isMoving())Thread.yield();
-		 pilot.rotate(-90);
+		 pilot.rotate(-720);
 		 pilot.rotate(270);
-		 pilot.steer(-50,180,true); // turn 180 degrees to the right
-
-		 pilot.steer(100);          // turns with left wheel stationary
-		 Delay.msDelay(1000);
 		 pilot.stop();
 	}
 	
